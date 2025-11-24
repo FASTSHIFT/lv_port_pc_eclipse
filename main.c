ba7e0683b50c13b754a6488747bad1f9fb1c8655
pengyiqiang <pengyiqiang@xiaomi.com>
2025-11-24 22:33:15 +0800

    chore(main): add mouse back

--- main.c
+++ main.c
@@ -170,9 +170,9 @@ static lv_disp_t* hal_init(int32_t w, int32_t h)
 {
 #if LV_USE_SDL
     lv_disp_t* disp = lv_sdl_window_create(w, h);
-    // lv_indev_t* mouse = lv_sdl_mouse_create();
-    // lv_indev_set_group(mouse, lv_group_get_default());
-    // lv_indev_set_display(mouse, disp);
+    lv_indev_t* mouse = lv_sdl_mouse_create();
+    lv_indev_set_group(mouse, lv_group_get_default());
+    lv_indev_set_display(mouse, disp);
 
     lv_indev_t* keyboard = lv_sdl_keyboard_create();
     lv_indev_set_display(keyboard, disp);
