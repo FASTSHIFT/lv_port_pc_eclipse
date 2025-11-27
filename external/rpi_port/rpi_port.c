14506bd2403ba5f2b796cb9d1115e9a90d8537e7
pengyiqiang <pengyiqiang@xiaomi.com>
2025-11-27 14:00:53 +0800

    fix(CMakeLists): adapt new BCM library location
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- external/rpi_port/rpi_port.c
+++ external/rpi_port/rpi_port.c
@@ -31,6 +31,14 @@
  *      TYPEDEFS
  **********************/
 
+#ifdef USE_EGL_DISPMANX_WINDOW_T
+typedef struct {
+   DISPMANX_ELEMENT_HANDLE_T element;
+   int width;   /* This is necessary because dispmanx elements are not queriable. */
+   int height;
+} EGL_DISPMANX_WINDOW_T;
+#endif
+
 /**********************
  *  STATIC PROTOTYPES
  **********************/
@@ -172,7 +180,7 @@ static int init_egl(uint32_t width, uint32_t height)
     nativewindow.height = height;
     vc_dispmanx_update_submit_sync(dispman_update);
 
-    surface = eglCreateWindowSurface(display, config, &nativewindow, NULL);
+    surface = eglCreateWindowSurface(display, config, (EGLNativeWindowType)&nativewindow, NULL);
     if (surface == EGL_NO_SURFACE) {
         LV_LOG_ERROR("Failed to create EGL surface: %d", eglGetError());
         return -1;
